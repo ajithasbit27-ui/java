8f5ff88fd4826f41db30739a625008478517a654
ajithasbit27-ui <ajithas_bit27@mepcoeng.ac.in>
2026-08-07 11:47:19 +0530

    Time: 0 ms (100%), Space: 8.7 MB (54%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.c
@@ -0,0 +1,9 @@
+int lengthOfLastWord(char* s) {
+   int len = 0, i = strlen(s) - 1;
+    while (i >= 0 && s[i] == ' ') i--;
+    while (i >= 0 && s[i] != ' ') {
+        len++;
+        i--;
+    }
+    return len; 
+}
